dd917b7e7524cbcbd1b4d07e41ba11e36a78d507
VALA9GALA <147095924+VALA9GALA@users.noreply.github.com>
2024-02-19 18:13:28 -0500

    added footer/links to project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-    <h1>WEATHER REACT</h1>
+        <h1>WEATHER REACT</h1>
       </header>
+      <footer>
+        Coded by{" "}
+        <a
+          href="https://valagala.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          VALAGALA
+        </a>{" "}
+        See{" "}
+        <a
+          href="https://github.com/VALA9GALA/valagala-react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          HERE
+        </a>
+      </footer>
     </div>
   );
 }
-
-
