78c0e74f030d1061c5da2ed7dddc242bdecb1d2a
VALA9GALA <147095924+VALA9GALA@users.noreply.github.com>
2024-02-19 19:02:50 -0500

    added bootstrap and npm update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>WEATHER REACT</h1>
-      </header>
-      <footer>
-        Coded by{" "}
-        <a
-          href="https://valagala.netlify.app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          VALAGALA
-        </a>{" "}
-        See{" "}
-        <a
-          href="https://github.com/VALA9GALA/valagala-react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          HERE
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>WEATHER REACT</h1>
+        </header>
+        <footer>
+          Coded by{" "}
+          <a
+            href="https://valagala.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            VALAGALA
+          </a>{" "}
+          See{" "}
+          <a
+            href="https://github.com/VALA9GALA/valagala-react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            HERE
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
