cb870e988c0b92361fa064fcf3d69b914d3b779d
VALA9GALA <147095924+VALA9GALA@users.noreply.github.com>
2024-04-15 20:08:29 -0400

    style updates

--- src/App.js
+++ src/App.js
@@ -6,25 +6,39 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
-        <footer>
-          Coded by{" "}
-          <a
-            href="https://valagala.netlify.app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            VALAGALA
-          </a>{" "}
-          See{" "}
-          <a
-            href="https://github.com/VALA9GALA/valagala-react-weather-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            HERE
-          </a>
-        </footer>
+        <div className="row ms-5 me-5 ps-5 pe-5">
+          <div className="col-md ms-5 ps-5 mt-5">
+            <h1 className="mt-5">
+              VALAGALA
+              <br />
+              <br />
+              <span className="title">WEATHER</span>
+              <br />
+              <span className="title">REACT</span>
+            </h1>
+          </div>
+          <div className="col-md me-5 pe-5">
+            <Weather defaultCity="New York" />
+          </div>
+          <footer>
+            Coded by{" "}
+            <a
+              href="https://valagala.netlify.app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              VALAGALA
+            </a>{" "}
+            See{" "}
+            <a
+              href="https://github.com/VALA9GALA/valagala-react-weather-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              HERE
+            </a>
+          </footer>
+        </div>
       </div>
     </div>
   );
